022c79a1b0a95bd08fe5f9b2e8c64421cee99b52
Hiraj Gajera <gajerahiraj@gmail.com>
2025-05-22 18:41:08 +0530

    hiraj

--- api/server.js
+++ api/server.js
@@ -1,5 +1,5 @@
 const dotenv = require('dotenv').config();
-const app = require('../app'); // ✅ Go up one level
+const app = require('../app'); // ✅ Go up by one level
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
